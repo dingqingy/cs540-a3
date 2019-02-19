b2247568e2495c2f26b184cdee0a8157b688d808
Dingqing Yang <williamyang1203@gmail.com>
2019-02-18 19:19:24 -0800

    add 1.3.4

--- code/example_generative.jl
+++ code/example_generative.jl
@@ -5,19 +5,19 @@ data = load("../data/gaussNoise.jld")
 (X,y,Xtest,ytest) = (data["X"],data["y"],data["Xtest"],data["ytest"])
 
 # Fit a KNN classifier
-# k = 3
-# include("knn.jl")
-# model = knn(X,y,k)
+k = 3
+include("knn.jl")
+model = knn(X,y,k)
 
 # # Evaluate training error
-# yhat = model.predict(X)
-# trainError = mean(yhat .!= y)
-# @printf("Train Error with %d-nearest neighbours: %.3f\n",k,trainError)
+yhat = model.predict(X)
+trainError = mean(yhat .!= y)
+@printf("Train Error with %d-nearest neighbours: %.3f\n",k,trainError)
 
 # Evaluate test error
-# yhat = model.predict(Xtest)
-# testError = mean(yhat .!= ytest)
-# @printf("Test Error with %d-nearest neighbours: %.3f\n",k,testError)
+yhat = model.predict(Xtest)
+testError = mean(yhat .!= ytest)
+@printf("Test Error with %d-nearest neighbours: %.3f\n",k,testError)
 
 
 # Fit a GDA classifier
@@ -28,3 +28,12 @@ model = gda(X,y)
 yhat = model.predict(Xtest)
 testError = mean(yhat .!= ytest)
 @printf("Test Error with GDA: %.3f\n",testError)
+
+# Fit a GDA classifier
+include("tda.jl")
+model = tda(X,y)
+
+# Evaluate test error
+yhat = model.predict(Xtest)
+testError = mean(yhat .!= ytest)
+@printf("Test Error with TDA: %.3f\n",testError)
---
+++ code/tda.jl
@@ -0,0 +1,38 @@
+include("misc.jl") # Includes mode function and GenericModel typedef
+include("studentT.jl")
+
+function tda_predict(Xhat,X,y)
+  (n,d) = size(X)
+  (t,d) = size(Xhat)
+  k = maximum(y)
+
+  # calculate MLP for p(y)
+  theta = zeros(k)
+  for c = 1:k
+    theta[c] = sum(y .== c) / n
+  end
+
+  # fit k submodels
+  subModel = Array{DensityModel}(undef,k)
+  for c in 1:k
+    subModel[c] = studentT(X[y .== c, :])
+  end
+
+  pdf_mat = zeros(t, k)
+  for c in 1:k
+    pdf_mat[:,c] = log.(subModel[c].pdf(Xhat)) .+ log(theta[c])
+  end
+
+  yhat = zeros(t)  
+  for i in 1:t
+    yhat[i] = argmax(pdf_mat[i, :])
+  end 
+
+  return yhat
+end
+
+function tda(X,y)
+	# Implementation of GDA classifier
+  predict(Xhat) = tda_predict(Xhat,X,y)
+  return GenericModel(predict)
+end
